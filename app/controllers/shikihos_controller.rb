ddf6b9d11e16adf9d8efa80314985abd4a084642
haru <shun_h@hotmail.com>
2020-06-09 14:21:34 +0900

    一覧画面の作成

--- app/controllers/shikihos_controller.rb
+++ app/controllers/shikihos_controller.rb
@@ -4,7 +4,33 @@ class ShikihosController < ApplicationController
   # GET /shikihos
   # GET /shikihos.json
   def index
+    @search_params = params
+    @year = params['year'].present? ? params['year'] : 2020
+    quarter = params['quarter'].present? ? params['quarter'] : 1
+
     @shikihos = Shikiho.all
+    @shikihos = @shikihos.year_equal(@year)
+    @shikihos = @shikihos.quarter_equal(quarter)
+    @shikihos = @shikihos.code_like(params[:code]) if params[:code].present?
+    @shikihos = @shikihos.comp_name_like(params[:comp_name]) if params[:comp_name].present?
+    @shikihos = @shikihos.topic_like(params[:topic]) if params[:topic].present?
+    @shikihos = @shikihos.feature_like(params[:feature]) if params[:feature].present?
+    @shikihos = @shikihos.outlook_like(params[:outlook]) if params[:outlook].present?
+    @shikihos = @shikihos.order(:code).page(params[:page]).per(20)
+    @quarter = self.get_quarter_from_num(quarter)
+  end
+
+  def get_quarter_from_num(quarter)
+    case quarter
+    when '1' then
+      '春'
+    when '2' then
+      '夏'
+    when '3' then
+      '秋'
+    when '4' then
+      '冬'
+    end
   end
 
   # GET /shikihos/1
--- app/models/shikiho.rb
+++ app/models/shikiho.rb
@@ -1,2 +1,10 @@
 class Shikiho < ApplicationRecord
+    scope :year_equal, -> (year) { where(year: year) if year.present? }
+    scope :quarter_equal, -> (quarter) { where(quarter: quarter) if quarter.present? }
+    scope :code_like, -> (code) { where('code LIKE ?', "%#{code}%") if code.present? }
+    scope :code_equal, -> (code) { where(code: code) if code.present? }
+    scope :comp_name_like, -> (comp_name) { where('comp_name LIKE ?', "%#{comp_name}%") if comp_name.present? }
+    scope :feature_like, -> (feature) { where('feature LIKE ?', "%#{feature}%") if feature.present? }
+    scope :topic_like, -> (topic) { where('topic LIKE ?', "%#{topic}%") if topic.present? }
+    scope :outlook_like, -> (outlook) { where('outlook LIKE ?', "%#{outlook}%") if outlook.present? }
 end
